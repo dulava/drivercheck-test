49bf750f7747a2411a0fe3a5a37bc7ec237ee280
Dragoslav Dulava <dulava@yahoo.com>
2018-05-14 01:36:05 -0400

    something works

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 // server.js
 
+
     // set up ========================
    var express  = require('express');
   var app      = express();                               // create our app w/ express
-  var mongoose = require('mongoose');                     // mongoose for mongodb
     var morgan = require('morgan');             // log requests to the console (express4)
    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
     // configuration =================
-	//const MongoClient = require('mongodb').MongoClient
-	
-    mongoose.connect('mongodb://localhost:27017/drivercheck');     // connect to mongoDB database on modulus.io
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
 
+var db
+
+MongoClient.connect('mongodb://localhost:27017/drivercheck', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('drivercheck') // whatever your database name is
+  app.listen(8070, () => {
+    console.log('listening on 8070 mongo')
+  })
+})
+	
     app.use(express.static(__dirname ));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -20,81 +29,49 @@
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    // listen (start app with node server.js) ======================================  
-   	app.listen(8080);
-    console.log("App listening on port 8080");
-
-	
-  // define model =================
-
-
-    var Clients = mongoose.model('clients', {
-       text : String
-    });
-
-	//console.log(Clients);
 	
 // routes ======================================================================
 
-    // api ---------------------------------------------------------------------
-	
-    // get all todos
     app.get('/clients', function(req, res) {
-		//db.collection('clients').find();
-		console.log("got get clients");
+		db.collection('clients').find().toArray((err, clients) => {
+			if (err) return console.log(err)
+			res.json(clients);
+		})
 		
-		res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
-        // use mongoose to get all todos in the database
-        Clients.find(function(err, clients) {
-			console.log( clients );
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(clients); // return all todos in JSON format
-			
-        });
     });
 
-    // create todo and send back all todos after creation
+
     app.post('/clients', function(req, res) {
+		db.collection('clients').save(req.body, (err, result) => {
+			if (err) return console.log(err)
+			console.log('saved to database')
+		})
+		
+		db.collection('clients').find().toArray((err, clients) => {
+			if (err) return console.log(err)
+			res.json(clients);
+		})
 
-        // create a todo, information comes from AJAX request from Angular
-        Clients.create({
-            text : req.body.text,
-            done : false
-        }, function(err, client) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Clients.find(function(err, clients) {
-                if (err)
-                    res.send(err)
-                res.json(clients);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/clients/:client_id', function(req, res) {
-        Clients.remove({
-            _id : req.params.client_id
-        }, function(err, client) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Clients.find(function(err, clients) {
-                if (err)
-                    res.send(err)
-                res.json(clients);
-            });
-        });
-    });
+	});
 	
-	// application -------------------------------------------------------------
+	app.delete('/clients/:client_id', (req, res) => {
+		console.log(req.params.client_id);
+		var client_id = req.params.client_id;
+		
+		var query = db
+		.collection('clients')
+		.remove( {_id: ObjectID.createFromHexString( client_id ) },
+		(err, clients) => {
+			if (err) return console.log(err)
+			console.log(clients);
+		})
+		
+		db.collection('clients').find().toArray((err, clients) => {
+			if (err) return console.log(err)
+			res.json(clients);
+		})
+	});
+	
     app.get('*', function(req, res) {
         res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
